813f89a25ef4d9fa7024f60d0e30ece1d8374fef
surya_nelakanti <nelakantisuryateja@gmail.com>
2024-04-19 23:52:33 +0530

    updated docs, changed server name

--- backend/api_engine/asgi.py
+++ backend/api_engine/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for collab_engine project.
+ASGI config for api_engine project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "collab_engine.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "api_engine.settings")
 
 application = get_asgi_application()
