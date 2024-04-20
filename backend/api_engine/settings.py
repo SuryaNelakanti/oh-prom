1cbea7545e12c8ff1f24b1db830aa51f5b33cffa
surya_nelakanti <nelakantisuryateja@gmail.com>
2024-04-20 12:26:14 +0530

    removed static files, updated settings

--- backend/api_engine/settings.py
+++ backend/api_engine/settings.py
@@ -37,7 +37,6 @@ INSTALLED_APPS = [
     "django.contrib.contenttypes",
     "django.contrib.sessions",
     "django.contrib.messages",
-    "django.contrib.staticfiles",
     "rest_framework",
 ]
 
@@ -131,8 +130,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
-STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
-STATIC_URL = "static/"
+# STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
+# STATIC_URL = "static/"
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
--- backend/docker-entrypoint.sh
+++ backend/docker-entrypoint.sh
@@ -2,7 +2,4 @@
 
 python manage.py migrate --noinput || exit 1
 
-# Adds reference/link of all static files to a single file.
-python manage.py collectstatic --noinput --clear --link
-
 exec "$@"
