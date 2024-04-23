04ac88c54eed0daf8919bbe61d1d54d8ebe17d25
surya_nelakanti <nelakantisuryateja@gmail.com>
2024-04-23 20:16:35 +0530

    small fixes

--- frontend/src/routes/index.tsx
+++ frontend/src/routes/index.tsx
@@ -1,4 +1,4 @@
 export { App } from './app';
 export { ErrorPage } from './util-pages/error-page';
-export { Project as ProjectDetails } from './project/project';
+export { Project } from './project/project';
 export { Profile } from './profile/profile';
